a9e57a54b615a606429008988c4b9a090572a7eb
SetsunaiCodes <Nicojin.M0SIC@hotmail.com>
2023-02-16 05:30:48 +0100

    finished the project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@ import "ionicons";
 import Muuri from "muuri";
 
 const grid = new Muuri(".grid", {
-  dragEnabled: true,
+  dragEnabled: true
 });
 document
   .getElementById("collectionhoritem")
@@ -22,26 +22,32 @@ document
 document
   .getElementById("angeboteveritem")
   .addEventListener("click", angebotever);
-document.getElementById("timerhoritem").addEventListener("click", timerhor);
-document.getElementById("timerveritem").addEventListener("click", timerver);
+document.getElementById("eventshoritem").addEventListener("click", timerhor);
+document.getElementById("eventsveritem").addEventListener("click", timerver);
+document.getElementById("hintergrundchangeclose").addEventListener("click", hintergrundchangeclose);
+document.getElementById("default3").addEventListener("click",bg3);
+document.getElementById("default1").addEventListener("click",bg1);
+document.getElementById("default2").addEventListener("click",bg2);
+document.getElementById("residentevil").addEventListener("click",re);
+document.getElementById("hades").addEventListener("click",hades);
+document.getElementById("nomanssky").addEventListener("click",nomanssky);
+document.getElementById("gowwallpaper1").addEventListener("click",gow1);
+document.getElementById("gowwallpaper2").addEventListener("click",gow2);
+document.getElementById("melinda").addEventListener("click",melinda);
+document.getElementById("hintergrundchange").addEventListener("click",hintergrundchange);
+
 function collectionhor() {
   if (this.classList.contains("headericon")) {
-    var collectionhorelement = document.querySelectorAll(
-      ".grid .collectionhor"
-    );
-    collectionhorelement.forEach(function (collectionhor) {
-      collectionhor.classList.remove("hider");
-    });
+    var collectionhor = document.getElementById("collectionhor");
+    collectionhor.classList.remove("hider");
 
     this.classList.remove("headericon");
     this.classList.add("headericonused");
   } else {
-    var collectionhorelement = document.querySelectorAll(
-      ".grid #collectionhor"
-    );
-    items.forEach(function (collectionhor) {
-      collectionhor.classList.add("hider");
-    });
+    var collectionhor = document.getElementById("collectionhor");
+    collectionhor.classList.add("hider");
+
+
     this.classList.remove("headericonused");
     this.classList.add("headericon");
   }
@@ -77,16 +83,14 @@ function friendlisthor() {
 function friendlistver() {
   if (this.classList.contains("headericon")) {
     var friendlistverelement = document.getElementById("friendlistver");
-    const sortlayer = document.getElementsByClassName("sortlayer");
-    friendlistverelement.style.opacity = "0";
-    friendlistverelement.style.transform = "scale(0.5)";
-    sortlayer.stlye.witdh = "0px";
-    sortlayer.stlye.height = "643px";
+    friendlistverelement.classList.remove("hider");
+
     this.classList.remove("headericon");
     this.classList.add("headericonused");
   } else {
     var friendlistverelement = document.getElementById("friendlistver");
     friendlistverelement.classList.add("hider");
+
     this.classList.remove("headericonused");
     this.classList.add("headericon");
   }
@@ -121,13 +125,13 @@ function angebotever() {
 }
 function timerhor() {
   if (this.classList.contains("headericon")) {
-    var timerhorelement = document.getElementById("timerhor");
+    var timerhorelement = document.getElementById("eventshor");
     timerhorelement.classList.remove("hider");
 
     this.classList.remove("headericon");
     this.classList.add("headericonused");
   } else {
-    var timerhorelement = document.getElementById("timerhor");
+    var timerhorelement = document.getElementById("eventshor");
     timerhorelement.classList.add("hider");
     this.classList.remove("headericonused");
     this.classList.add("headericon");
@@ -135,15 +139,60 @@ function timerhor() {
 }
 function timerver() {
   if (this.classList.contains("headericon")) {
-    var timerverelement = document.getElementById("timerver");
+    var timerverelement = document.getElementById("eventsver");
     timerverelement.classList.remove("hider");
 
     this.classList.remove("headericon");
     this.classList.add("headericonused");
   } else {
-    var timerverelement = document.getElementById("timerver");
+    var timerverelement = document.getElementById("eventsver");
     timerverelement.classList.add("hider");
     this.classList.remove("headericonused");
     this.classList.add("headericon");
   }
+}
+function hintergrundchangeclose(){
+  var bgchangeelement = document.getElementById("bgchange");
+    bgchangeelement.classList.add("hider");
+}
+function bg1(){
+  const sortlayerbg = document.getElementById("sortlayer");
+  sortlayerbg.classList.add("default1");
+}
+function bg3(){
+  const sortlayerbg = document.getElementById("sortlayer");
+  sortlayerbg.classList.add("default3");
+}
+function bg2(){
+  const sortlayerbg = document.getElementById("sortlayer");
+  sortlayerbg.classList.add("default2");
+}
+function re(){
+  const sortlayerbg = document.getElementById("sortlayer");
+  sortlayerbg.classList.add("residentevil");
+}
+function hades(){
+  const sortlayerbg = document.getElementById("sortlayer");
+  sortlayerbg.classList.add("hades");
+}
+function nomanssky(){
+  const sortlayerbg = document.getElementById("sortlayer");
+  sortlayerbg.classList.add("nomanssky");
+}
+function melinda(){
+  const sortlayerbg = document.getElementById("sortlayer");
+  sortlayerbg.classList.add("melinda");
+}
+function gow1(){
+  const sortlayerbg = document.getElementById("sortlayer");
+  sortlayerbg.classList.add("gowwallpaper1");
+}
+function gow2(){
+  const sortlayerbg = document.getElementById("sortlayer");
+  sortlayerbg.classList.add("gowwallpaper2");
+}
+
+function hintergrundchange(){
+  var bgchangeelement = document.getElementById("bgchange");
+    bgchangeelement.classList.remove("hider");
 }
